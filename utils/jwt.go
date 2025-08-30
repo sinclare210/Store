2efce7287625f0898a9f73a6b3ab6ba7098beb05
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-30 10:53:57 +0100

    .env

---
+++ utils/jwt.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(email string, userId int64)(string, error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Email":email,
+		"User_Id":userId,
+		"exp": time.Now().Add(time.Hour * 2).Unix(),
+	})
+
+	return token.SignedString(secretKey)
+}
